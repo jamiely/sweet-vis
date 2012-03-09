e899773c451c2b6b91b9adc7a55791aea33a9006
Jamie Ly <jamie.ly@gmail.com>
2012-03-08 21:57:41 -0500

    Map is now colored based on effective retirement age

--- choropleth.js
+++ choropleth.js
@@ -1,37 +1,58 @@
 var data; // loaded asynchronously
 
 var xy = d3.geo.equirectangular()
-    .scale(1000),
+    .scale(900),
   path = d3.geo.path().projection(xy)
 
 var svg = d3.select("#chart")
   .append("svg");
 
-var counties = svg.append("g")
-    .attr("id", "counties")
-    .attr("class", "Blues");
-
 var states = svg.append("g")
-    .attr("id", "states");
+    .attr("id", "countries")
+    .attr("class", "Blues");
 
 d3.json("world-countries.json", function(json) {
   states.selectAll("path")
       .data(json.features)
     .enter().append("path")
       .attr("d", path);
-
-  states.selectAll("path")
-    .style("fill", "#F00");
 });
 
-d3.json("eff_v_official_retirement_age.json", function(csv) {
-  data = csv;
+d3.json("eff_v_official_retirement_age.json", function(json) {
+  data = json;
+  var dataValues = d3.values(data);
+
+  var funParseProp = function(propName) {
+    return dataValues.map(function(v) { return parseFloat(v[propName]); });
+  };
+
+  var dataEff = funParseProp('effective'),
+    dataOff = funParseProp('official'),
+    min = d3.min(dataEff),
+    max = d3.max(dataEff);
+
+  console.log("Min: " + min + "\nMax: " + max + "\n");
+
+  var curryQuantize = function(d) {
+    return quantize(min, max, d);
+  };
   states.selectAll("path")
-    .style("fill", determine);
+    .attr("class", curryQuantize);
 });
 
-function quantize(d) {
-  return "q" + Math.min(8, ~~(data[d.id] * 9 / 12)) + "-9";
+function quantize(min, max, d) {
+  var name = d.properties.name;
+  if(data[name]) {
+    // var className = "q" + Math.min(8, ~~(data[d.properties.name.effective] * 9 / 12)) + "-9";
+    var value = parseFloat(data[name].effective);
+    var className = 'q' + (Math.floor(9 * (value - min)/(max-min))) + '-9'
+    console.log(className);
+    return className;
+  }
+  else {
+    console.log('missing');
+    return "missing";
+  }
 }
 
 function determine(d) {
@@ -40,7 +61,9 @@ function determine(d) {
     return "#0F0";
   }
   else {
-    return "#000";
+    return "#EEE";
   }
 }
+
+
 
